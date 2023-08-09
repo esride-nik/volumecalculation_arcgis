851f5c07a5f136976fd1ab29046e6d10053bd326
Niklas Köhn <n.koehn@esri.de>
2023-08-09 11:59:28 +0200

    2 bands from TimeImageLayer yay!

--- examples/examples/VolumeCalc.tsx
+++ examples/examples/VolumeCalc.tsx
@@ -1,6 +1,7 @@
 import * as promiseUtils from '@arcgis/core/core/promiseUtils';
 import Extent from '@arcgis/core/geometry/Extent';
 import ImageryLayer from '@arcgis/core/layers/ImageryLayer';
+import ImageryTileLayer from '@arcgis/core/layers/ImageryTileLayer';
 import MapView from '@arcgis/core/views/MapView';
 import Expand from '@arcgis/core/widgets/Expand';
 import LayerList from '@arcgis/core/widgets/LayerList';
@@ -9,6 +10,7 @@ import { useMemo, useState } from 'react';
 
 import { ArcMapView, ArcUI, ArcWidget, useMapView } from '../../src';
 import { ArcImageryLayer } from '../../src/components/ArcLayer/generated/ArcImageryLayer';
+import { ArcImageryTileLayer } from '../../src/components/ArcLayer/generated/ArcImageryTileLayer';
 
 export default function VolumeCalc() {
   return (
@@ -94,45 +96,51 @@ function Layers() {
         spatialReference: { wkid: 102_100 },
       });
       console.log('requestExtent', requestExtent);
-      const fetchedPixels = await (layer as ImageryLayer).fetchImage(
-        requestExtent,
-        10,
-        10
-      );
-      console.log(
-        'fetchedPixels',
-        fetchedPixels,
-        fetchedPixels.pixelData.pixelBlock.pixels
-      );
 
-      const compare01 = fetchedPixels.pixelData.pixelBlock.pixels[0].every((val: any, index: number) => val === fetchedPixels.pixelData.pixelBlock.pixels[1][index]);
-      const compare12 = fetchedPixels.pixelData.pixelBlock.pixels[1].every((val: any, index: number) => val === fetchedPixels.pixelData.pixelBlock.pixels[2][index]);
-      console.log('compare', compare01, compare12);
+      // const fetchedPixels = await (layer as ImageryLayer).fetchImage(
+      //   requestExtent,
+      //   10,
+      //   10
+      // );
+      const fetchedPixels = {
+        pixelData: await (layer as unknown as ImageryTileLayer).fetchPixels(
+          requestExtent,
+          10,
+          10
+        ),
+      };
+      console.log('fetchedPixels', fetchedPixels);
+
+      const compare01 = fetchedPixels.pixelData.pixelBlock.pixels[0].every(
+        (val: any, index: number) =>
+          val === fetchedPixels.pixelData.pixelBlock.pixels[1][index]
+      );
+      console.log('compare', compare01);
 
       // return (layer as ImageryLayer)
       //   .identify({ geometry: point })
       //   .then((results: any) => {
       //     console.log('identify results', results);
 
-      //     // if (results.value) {
-      //     //   document.querySelector('#instruction').style.display = 'none';
-      //     //   // Update the spectral chart for the clicked location on the image
-      //     //   spectralChart.data.datasets[0].data = [];
-      //     //   spectralChart.data.datasets[0].data = results.value;
-      //     //   spectralChart.update(0);
-      //     //   if (chartDiv.style.display === 'none') {
-      //     //     chartDiv.style.display = 'block';
-      //     //   }
-      //     //   document.querySelector(
-      //     //     '#ndviValueDiv'
-      //     //   ).innerHTML = `Processed NDVI value:  ${
-      //     //     (results.processedValue - 100) / 100
-      //     //   }`;
-      //     // } else {
-      //     //   document.querySelector('#instruction').style.display = 'block';
-      //     //   chartDiv.style.display = 'none';
-      //     //   document.querySelector('#ndviValueDiv').innerHTML = '';
-      //     // }
+      // if (results.value) {
+      //   document.querySelector('#instruction').style.display = 'none';
+      //   // Update the spectral chart for the clicked location on the image
+      //   spectralChart.data.datasets[0].data = [];
+      //   spectralChart.data.datasets[0].data = results.value;
+      //   spectralChart.update(0);
+      //   if (chartDiv.style.display === 'none') {
+      //     chartDiv.style.display = 'block';
+      //   }
+      //   document.querySelector(
+      //     '#ndviValueDiv'
+      //   ).innerHTML = `Processed NDVI value:  ${
+      //     (results.processedValue - 100) / 100
+      //   }`;
+      // } else {
+      //   document.querySelector('#instruction').style.display = 'block';
+      //   chartDiv.style.display = 'none';
+      //   document.querySelector('#ndviValueDiv').innerHTML = '';
+      // }
       //   })
       //   .catch((error) => {
       //     if (!promiseUtils.isAbortError(error)) {
@@ -220,24 +228,19 @@ function Layers() {
 
   return (
     <>
-      {/* <ArcMapView
-        map={{ portalItem: { id: 'def32d44bec8442ba2ef612bb35ad7bb' } }}
-        zoom={15}
-        onViewCreated={setMapView}
-        style={{ height: '100vh' }}
-      >
-        <ArcUI position="top-right">
-          <ArcWidget widget={layerList} />
-        </ArcUI>
-
-        <ArcUI position="bottom-right">
-          <ArcWidget widget={legend} />
-        </ArcUI>
-      </ArcMapView> */}
-
-      <ArcImageryLayer
+      {/* <ArcImageryLayer
         layerProps={{
           url: 'https://iservices.arcgis.com/OLiydejKCZTGhvWg/arcgis/rest/services/VarzeadoLopesMineDemo_CVL_DRMINA_20180626_DSMDynamicImagery/ImageServer', // ImageryLayer
+          opacity: 0.9,
+          // TODO: pixelFilter only returns one band! why?
+          // pixelFilter: pixelFilterFunction,
+        }}
+        eventHandlers={{ 'layerview-create': onImgViewCreated }}
+      /> */}
+
+      <ArcImageryTileLayer
+        layerProps={{
+          url: 'https://tiledimageservices.arcgis.com/OLiydejKCZTGhvWg/arcgis/rest/services/VarzeaMin_DOMs/ImageServer', // tiled ImageryLayer
           opacity: 0.9,
           // TODO: pixelFilter only returns one band! why?
           // pixelFilter: pixelFilterFunction,
