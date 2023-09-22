bce656e21726d42775d232d92289e4388e724c8d
Niklas Köhn <n.koehn@esri.de>
2023-09-22 10:00:49 +0200

    rasterInfo.hasMultidimensionalTranspose

--- examples/examples/VolumeCalc.tsx
+++ examples/examples/VolumeCalc.tsx
@@ -271,6 +271,7 @@ function Layers() {
       const idResult = await imgLayer.identify({geometry: point});
       console.log("idResult", idResult);
       // TODO: How to get data from [or into via AGP] RasterIdentifyResult.dataSeries https://developers.arcgis.com/javascript/latest/api-reference/esri-layers-ImageryTileLayer.html#RasterIdentifyResult
+      // TODO: Check the following flag to see if dataSeries is there: layer.rasterInfo.hasMultidimensionalTranspose (not documented!!) => https://developers.arcgis.com/javascript/latest/api-reference/esri-layers-ImageryTileLayer.html#rasterInfo
 
       /*
 {"spatialReference":{"wkid":102100},"xmin":-4891674.043387455,"ymin":-2307140.978060443,"xmax":-4891516.043387455,"ymax":-2306982.978060443}
